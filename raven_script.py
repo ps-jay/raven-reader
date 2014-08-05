46972781ea1d3f03696de6f6a526b82f3d6d6da5
rub-a-dub-dub <rub-a-dub-dub@users.noreply.github.com>
2014-08-04 19:16:29 -0700

    Changed the main exec file to just call out the class, wrapping it with some parameter validation logic.

--- raven_script.py
+++ raven_script.py
@@ -1,76 +1,13 @@
 #!/usr/bin/env python
 
-import sys
-import serial
-import time
-import xml.etree.ElementTree as ET
-import re
-import logging as log
 import argparse
-import paho.mqtt.client as mqtt
 import signal
+from RAVEnMQTT import RAVEnMQTT as raven
+import sys
+import logging as log
 
-# This is the serial object (held globally so the Ctrl+C interrupt handler will work)
-ser = ""
-
-# Various Regex's
-reStartTag = re.compile('^<[a-zA-Z0-9]+>') # to find a start XML tag (at very beginning of line)
-reEndTag = re.compile('^<\/[a-zA-Z0-9]+>') # to find an end XML tag (at very beginning of line)
-
-def sendCommand(serialport, command):
-  '''Given a command it will be formatted in XML and written to serialport for RAVEn device'''
-  # Sends a simple command, such as initialize, get_instantaneous_demand, etc.
-  output = ("<Command>\n  <Name>%s</Name>\n</Command>" % command)
-  log.info("Issuing command: " + command)
-  serialport.write(output)
-  time.sleep(0.5) # allow this command to sink in
-
-def getInstantDemandKWh(xmltree):
-  '''Returns a single float value for the Demand from an Instantaneous Demand response from RAVEn'''
-  # Get the Instantaneous Demand
-  fDemand = float(int(xmltree.find('Demand').text,16))
-  fResult = calculateRAVEnNumber(xmltree, fDemand)
-  return fResult
-
-def calculateRAVEnNumber(xmltree, value):
-  '''Calculates a float value from RAVEn using Multiplier and Divisor in XML response'''
-  # Get calculation parameters from XML - Multiplier, Divisor
-  fDivisor = float(int(xmltree.find('Divisor').text,16))
-  fMultiplier = float(int(xmltree.find('Multiplier').text,16))
-  if (fMultiplier > 0 and fDivisor > 0):
-    fResult = float( (value * fMultiplier) / fDivisor)
-  elif (fMultiplier > 0):
-    fResult = float(value * fMultiplier)
-  else: # (Divisor > 0) or anything else
-    fResult = float(value / fDivisor)
-  return fResult*1000
-
-# Callback for MQTT Client
-#TODO: This isn't working??
-def on_connect(client, userdata, rc):
-  '''Event handler for when the MQTT connection is made'''
-  if rc == 0:
-    log.info("Connected to server.")
-    client.subscribe("$SYS/#")
-    return
-  elif rc == 1:
-    log.critical("Connection to server refused - incorrect protocol version.")
-  elif rc == 2:
-    log.critical("Connection to server refused - invalid client identifier.")
-  elif rc == 3:
-    log.critical("Connection to server refused - server unavailable.")
-  elif rc == 4:
-    log.critical("Connection to server refused - bad username or password.")
-  elif rc == 5:
-    log.critical("Connection to server refused - not authorised.")
-  elif rc >=6 :
-    log.critical("Reserved code received!")
-  ser.close()
-  exit()
-
-#def on_publish(client, userdata, mid):
-#  '''Event handler for when the MQTT message is published'''
-#  print("Sent message")
+# This holds our RAVEn to MQTT class
+myWorker = None
 
 def argProcessing():
   '''Processes command line arguments'''
@@ -86,8 +23,12 @@ def argProcessing():
   return parser.parse_args()
 
 def exitSafely():
-  ser.close()
+  '''SIGINT (Ctrl+C) handler'''
+  global myWorker
+  if myWorker is not None: 
+    myWorker.close()
   log.info("Ctrl+C pressed. Exiting.")
+  exit(0)
 
 def main():
   # Process cmd line arguments
@@ -103,76 +44,19 @@ def main():
   if programArgs.logfile is not None:
     log.basicConfig(filename='raven.log',level=verbosityLevel)
 
+  # Initial log message
   log.info("Programme started.")
 
-  # Register exit handler
-  signal.signal(signal.SIGINT, exitSafely)
-  
-  # open serial port
-  try:
-    global ser
-    ser = serial.Serial(programArgs.device, 115200, serial.EIGHTBITS, serial.PARITY_NONE, timeout=0.5)
-    ser.close()
-    ser.open()
-    ser.flushInput()
-    ser.flushOutput()
-    log.info("connected to: " + ser.portstr)
-  except Exception as e:
-    log.critical("cannot open serial port: " + str(e))
-    exit()
-  
-  # send initialize command to RAVEn (pg.9 of XML API Doc)
-  #TODO: For some reason this command causes the error "Unknown command"?
-  #sendCommand(ser, "initialise" )
+  # Initialise the class 
+  global myWorker
+  myWorker = raven(programArgs.device, programArgs.host, programArgs.port, programArgs.u, programArgs.P, programArgs.topic)
+  if not myWorker.open():
+    log.critical("Couldn't access resources needed. Check logs for more information.")
+  else:
+    myWorker.run()
 
-  # setup mqtt connection
-  client = mqtt.Client();
-  client.on_connect = on_connect;
-  #client.on_publish = on_publish;
-  if programArgs.u is not None:
-    client.username_pw_set(programArgs.u, programArgs.P)
-  client.connect(programArgs.host, programArgs.port, 60)
-
-  # begin listening to RAVEn
-  rawxml = ""
-
-  while True:
-    # wait for /n terminated line on serial port (up to timeout)
-    rawline = ser.readline()
-    # remove null bytes that creep in immediately after connecting
-    rawline = rawline.strip('\0')
-    # only bother if this isn't a blank line
-    if len(rawline) > 0:
-      # start tag
-      if reStartTag.match(rawline):
-        rawxml = rawline
-        log.debug("Start XML Tag found: " + rawline)
-      # end tag
-      elif reEndTag.match(rawline):
-        rawxml = rawxml + rawline
-        log.debug("End XML Tag Fragment found: " + rawline)
-        try:
-          xmltree = ET.fromstring(rawxml)
-          if xmltree.tag == 'InstantaneousDemand':
-            client.publish(programArgs.topic, payload=getInstantDemandKWh(xmltree), qos=0)
-            log.debug(getInstantDemandKWh(xmltree))
-          else:
-            log.warning("*** Unrecognised (not implemented) XML Fragment")
-            log.warning(rawxml)
-        except Exception as e:
-          log.error("Exception triggered: " + str(e))
-        # reset rawxml
-        rawxml = ""
-      # if it starts with a space, it's inside the fragment
-      else:
-        rawxml = rawxml + rawline
-        log.debug("Normal inner XML Fragment: " + rawline)
-    else:
-      log.warning("Skipped an XML fragment since it was malformed.")
-
-  # never gets called?
-  ser.close()
-  log.info("Exiting.")
+    # Register exit handler
+    signal.signal(signal.SIGINT, exitSafely)
 
 if __name__ == '__main__':
   main()
